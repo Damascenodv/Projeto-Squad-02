141f2f248d638327bfa24486dea42b9bf659c5bb
João Damasceno <id629631@outlook.com>
2024-04-21 09:49:12 -0300

    relatorio fora da pagina

--- index.js
+++ index.js
@@ -12,13 +12,23 @@ function construirCalculo(){
     let numeroFiliais = parseInt(document.getElementById('numeroFiliais').value);
     let possuiPlanoEstrategioc = document.getElementById('possuiPlanoEstrategico').checked ? 1 : 0;
 
+    
+
+    let resultHorasAnlistaJunior = horasAnlistaJunior(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc);
+    let resultHorasAnlistaSenior = horasAnlistaSênior(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc);
+    let resultHorasEspecialista = horasEspecialista(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc);
+    let resultCustoHH = custoHH(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc);
+    let resultValorVenda = ValorVenda(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc);
+
+    salvarCalculolocalSotrage(resultHorasAnlistaJunior,resultHorasAnlistaSenior,resultHorasEspecialista,resultCustoHH,resultValorVenda);
+    
     let valoresDiv = document.getElementById('valores');
         valoresDiv.innerHTML = `
-            <p>Horas Anlista Junior: ${horasAnlistaJunior(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc).toFixed(1)}</p>
-            <p>Horas Anlista Sênior: ${horasAnlistaSênior(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc).toFixed(1)}</p>
-            <p>Horas Especialista: ${horasEspecialista(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc).toFixed(1)}</p>
-            <p>Custo HH: ${custoHH(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
-            <p>Valor Venda: ${ValorVenda(numServioresFisicos,numeroColaboradores,numeroSistemasUtilizados,numeroFiliais,possuiPlanoEstrategioc).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+            <p>Horas Anlista Junior: ${localStorage.getItem("HorasAnlistaJuniorF")}</p>
+            <p>Horas Anlista Sênior: ${localStorage.getItem("HorasAnlistaJuniorF")}</p>
+            <p>Horas Especialista: ${localStorage.getItem("HorasAnlistaJuniorF")}</p>
+            <p>Custo HH: ${localStorage.getItem("resultCustoHHF")}</p>
+            <p>Valor Venda: ${localStorage.getItem("resultValorVendaF")}</p>
         `;
 }
 
@@ -46,22 +56,38 @@ function ValorVenda(numServioresFisicos,numeroColaboradores,numeroSistemasUtiliz
 }
 
 function gerarPDF() {
-    window.location.href = "exemplo.html";
-    window.print();
+
+    const printWindow = window.open('relatorioModelo.html', '_blank');
+
+    printWindow.print();
 }
+function salvarCalculolocalSotrage(resultHorasAnlistaJunior,resultHorasAnlistaSenior,resultHorasEspecialista,resultCustoHH,resultValorVenda){
+    localStorage.setItem("HorasAnlistaJuniorF", resultHorasAnlistaJunior.toFixed(1));
+    localStorage.setItem("HorasAnlistaSeniorF", resultHorasAnlistaSenior.toFixed(1));
+    localStorage.setItem("HorasEspecialistaF", resultHorasEspecialista.toFixed(1));
+    localStorage.setItem("resultCustoHHF", resultCustoHH.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+    localStorage.setItem("resultValorVendaF", resultValorVenda.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+
+    localStorage.setItem("HorasAnlistaJunior", resultHorasAnlistaJunior);
+    localStorage.setItem("HorasAnlistaSenior", resultHorasAnlistaSenior);
+    localStorage.setItem("HorasEspecialista", resultHorasEspecialista);
+    localStorage.setItem("resultCustoHH", resultCustoHH);
+    localStorage.setItem("resultValorVenda", resultValorVenda);
+
+}
+
 function saveDataToLocalStorage() {
         
-    
 
-    localStorage.setItem("imposto", document.getElementById("imposto").value);
-    localStorage.setItem("lucro", document.getElementById("lucro").value);
-    localStorage.setItem("analistaJR", document.getElementById("analistaJR").value);
-    localStorage.setItem("analistaSr", document.getElementById("analistaSr").value);
-    localStorage.setItem("especialista", document.getElementById("especialista").value);
-
-    window.location.href = "index.html";
-    window.print();
+        localStorage.setItem("imposto", document.getElementById("imposto").value);
+        localStorage.setItem("lucro", document.getElementById("lucro").value);
+        localStorage.setItem("analistaJR", document.getElementById("analistaJR").value);
+        localStorage.setItem("analistaSr", document.getElementById("analistaSr").value);
+        localStorage.setItem("especialista", document.getElementById("especialista").value);
     
+        window.location.href = "index.html";
+        
+        
 }
 
 function limpar(){
