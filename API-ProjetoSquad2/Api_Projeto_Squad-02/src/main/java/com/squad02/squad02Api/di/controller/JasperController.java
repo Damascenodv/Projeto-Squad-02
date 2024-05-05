c3a79d3e10c7c1e2d858f10c90f189328efe4443
João Damasceno <id629631@outlook.com>
2024-05-05 19:22:02 -0300

    relatorio jasper

---
+++ API-ProjetoSquad2/Api_Projeto_Squad-02/src/main/java/com/squad02/squad02Api/di/controller/JasperController.java
@@ -0,0 +1,62 @@
+package com.squad02.squad02Api.di.controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+
+@Controller
+public class JasperController {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/report")
+    public void generateAndStreamReport(HttpServletResponse response) {
+        try {
+            ClassPathResource resource = new ClassPathResource("MyReports/Relatorio.jasper");
+            InputStream jasperStream = resource.getInputStream();
+            JasperReport jasperReport = (JasperReport) net.sf.jasperreports.engine.util.JRLoader
+                    .loadObject(jasperStream);
+
+            Map<String, Object> params = new HashMap<>();
+            Connection conn = dataSource.getConnection();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            JRPdfExporter exporter = new JRPdfExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(byteArrayOutputStream));
+            exporter.exportReport();
+
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=report.pdf");
+
+            response.getOutputStream().write(byteArrayOutputStream.toByteArray());
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+    }
+}
